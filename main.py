633d048297bae7b0560db0d580ab3934965ec507
Romigo97 <roma.kolyada939@gmail.com>
2025-02-10 21:10:14 +0300

    add a comparison function by language

--- main.py
+++ main.py
@@ -16,6 +16,32 @@ def get_vacancies_count():
     for vacancy in moscow_vacancies:
         print(vacancy['name'], vacancy['published_at'])
 
+def job_on_languages():
+    programming_languages = [
+        'JavaScript',
+        'Java',
+        'Python',
+        'Ruby',
+        'PHP',
+        'C++',
+        'C#',
+        'C',
+        'Go',
+        'Shell']
+    languages_count = dict()
+    url = 'https://api.hh.ru/vacancies'
+    
+    for language in programming_languages:
+        payload = {
+            'text': f'программист {language}',
+            'area': '1',
+            'period': '30'
+        }
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        vacancies = response.json()['found']
+        languages_count[language] = vacancies
+    print(languages_count)
 
 if __name__ == '__main__':
-    get_vacancies_count()
+    job_on_languages()
